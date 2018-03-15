659c1ceb2213721fddaf9eddfd744fec88860df1
thomas <toto61300@gmail.com>
2018-03-15 14:50:09 +0100

    Add src jwt

--- index.php
+++ index.php
@@ -11,9 +11,7 @@ header('Content-Type: application/json');
 include ("Vol/ManageBDD.php");
 include ("Vol/Engine.php");
 
-<<<<<<< HEAD
 
-=======
 use Firebase\JWT\JWT;
 require_once './php-jwt-master/src/JWT.php';
 $time =time();
@@ -26,7 +24,6 @@ $decode=JWT::decode($jvt,$Key,array('HS256'));
 print_r($jvt);
 echo '<br/><br/>';
 print_r($decode);
->>>>>>> f8ec30839223b3b0e5fe1a9b25b413398db31d17
 
 
 $engine = new Engine();
