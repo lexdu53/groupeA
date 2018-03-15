f010b580c81169f4ad8712e1775d20334459aefb
thomas <toto61300@gmail.com>
2018-03-15 15:17:47 +0100

    Update Authentification.php

--- Vol/Authentification.php
+++ Vol/Authentification.php
@@ -9,4 +9,28 @@
 class Authentification
 {
 
+    /**
+     *
+     */
+    function generateToken($key,$id,$user)
+    {
+        $Key = uniqid();//mémoriser la clé
+        $token = array('ini' => time(),
+            'exp' => time() + (60 * 60),
+            'id' => '1',
+            'user' => $user);
+        $jvt = JWT::encode($token, $Key);
+
+        return $jvt;
+    }
+
+    /**
+     * @param $token
+     */
+    function descryptToken($token,$key){
+
+        $decode=JWT::decode($token,$key,array('HS256'));
+        return $decode;
+
+    }
 }
--- index.php
+++ index.php
@@ -10,20 +10,12 @@ session_start();
 
 include ("Vol/ManageBDD.php");
 include ("Vol/Engine.php");
-
-
 use Firebase\JWT\JWT;
 require_once './php-jwt-master/src/JWT.php';
-$time =time();
-$Key ="exemple key";
-$token = array('ini'=>$time,
-    'exp'=>$time+(60*60),
-    'id'=>'1');
-$jvt=JWT::encode($token,$Key);
-$decode=JWT::decode($jvt,$Key,array('HS256'));
-print_r($jvt);
-echo '<br/><br/>';
-print_r($decode);
+
+
+
+
 
 
 $engine = new Engine();
