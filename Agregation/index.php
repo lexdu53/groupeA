670ab93bc260e38b335f8d13b04ce6c4750bf01c
thomas <toto61300@gmail.com>
2018-03-19 16:38:45 +0100

    finish Reservation vol

--- Agregation/index.php
+++ Agregation/index.php
@@ -54,7 +54,21 @@ if(!isset($_SESSION['tokenUser']) || $_SESSION['tokenUser'] == NULL || !isset($_
         return $objFromJson;
     }
 
+    function reservation_vol($fonction,$id,$NbPlaces)
+    {
+        $myURL = "https://www.arnaudride.fr/webservices/tp/index.php?function=".$fonction."&login=".$_SESSION['login']."&token=".$_SESSION['tokenUser']."&id=".$id."&NbPlaces=".$NbPlaces;
+
+        $jsonFromURL = file_get_contents($myURL);
+        $objFromJson = json_decode($jsonFromURL, true);
+
+        //print_r($myURL);
+        checkErreur($objFromJson);
+
+        return $objFromJson;
+    }
+
 	//print_r(affichertouslesvols("listallvol"));
-	print_r(affichertouslesvolsByVille("listallvol","Lyon","Paris"));
+	//print_r(affichertouslesvolsByVille("listallvol","Lyon","Paris"));
+    print_r(reservation_vol("reserv_vol",1,11));
 
 ?>
