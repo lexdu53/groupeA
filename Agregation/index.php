3a49ba64d741c77c3c2682ef660c866a8f221eb2
Arnaud RIDÉ <arnaud.ride@outlook.fr>
2018-03-19 17:17:16 +0100

    ajout form

--- Agregation/index.php
+++ Agregation/index.php
@@ -57,4 +57,22 @@ if(!isset($_SESSION['tokenUser']) || $_SESSION['tokenUser'] == NULL || !isset($_
 	//print_r(affichertouslesvols("listallvol"));
 	print_r(affichertouslesvolsByVille("listallvol","Lyon","Paris"));
 
+	if (isset($_POST['villeDepart']) && $_POST['villeDepart'] != NULL && isset($_POST['villeArrive']) && $_POST['villeArrive'] != NULL) {
+        // Si on a envoyer le formulaire pour rechercher des vols 
+        echo "test";
+		$arrayVols = affichertouslesvolsByVille("listallvol", $_POST['villeDepart'], $_POST['villeArrive']);
+		foreach ($arrayVols as $value){
+		?> 
+		<p>coucou</p>
+			<p><?php echo "ID :".$value["ID"]."   Ville départ: ".$value["Ville départ"]."   Ville arrivé: ".$value["Ville arrivé"]."   Date départ: ".$value["Date départ"]."   Date arrivé : ".$value["Date arrivé"]."   Places restante / Nombre de places total :".$value["Places restante / Nombre de places total"]."   Prix :".$value["Prix"];?><p>
+		<?php
+		}
+	}
 ?>
+
+<link rel="stylesheet" href="formoid_files/formoid1/formoid-flat-green.css" type="text/css" />
+<script type="text/javascript" src="formoid_files/formoid1/jquery.min.js"></script>
+<form class="formoid-flat-green" style="background-color:#FFFFFF;font-size:14px;font-family:'Lato', sans-serif;color:#666666;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Lister les vols</h2></div>
+	<div class="element-input"><label class="title">Ville de départ</label><input class="large" type="text" name="villeDepart" /></div>
+	<div class="element-input"><label class="title">Ville d'arrivé</label><input class="large" type="text" name="villeArrive" /></div>
+<div class="submit"><input type="submit" value="Rechercher"/></div></form><script type="text/javascript" src="formoid_files/formoid1/formoid-flat-green.js"></script>
